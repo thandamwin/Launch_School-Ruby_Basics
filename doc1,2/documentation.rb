83b2ef5afc66e0ed031b747629d917f48fe18b33
EC2 Default User <ec2-user@ip-172-31-25-194.us-west-1.compute.internal>
2020-10-06 09:25:19 +0000

    Documentation 1 and 2

---
+++ doc1,2/documentation.rb
@@ -0,0 +1,8 @@
+# 1. https://ruby-doc.org
+# 2. Select the core API link for your version of ruby. Select the "control expressions" link. Search within the page for "while" until you find the "while loop" discussion.
+# 3. The result of a while loop is nil unless break is used to supply a value.
+# 4. break returns nil when no arguments are passed to break, or the value of the argument when an argument is provided.
+# 5. You will find this information under the topic of literals. You can write large numbers by using underscores to separate groups of digits.
+# 6. Documentation for symbol literals is on the literals.rdoc page. :Jas
+# 7. https://ruby-doc.com/core-2.6.3/String.html
+# 8. #rjust
---
+++ doc1,2/documentation2.rb
@@ -0,0 +1,8 @@
+# 1. "xyz".upcase
+# 2. a.insert(3, 5, 6, 7)
+# 3. puts s.split.inspect
+# ["abc", "def", "ghi,jkl", "mno", "pqr,stu", "vwx", "yz"]
+# puts s.split(',').inspect
+# ["abc def ghi", "jkl mno pqr", "stu vwx yz"]
+# puts s.split(',', 2).inspect
+# ["abc def ghi", "jkl mno pqr,stu vwx yz"]
