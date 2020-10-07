eb372247bd0b63e026a2be573b527ec46f2f6f63
Thanda Myo Win <thandamwin@gmail.com>
2020-10-07 13:00:20 +0000

    Loops 2

---
+++ loops2/ex1.rb
@@ -0,0 +1,7 @@
+count = 0
+
+while count < 5
+      count += 1
+      puts "#{count} is odd." if count.odd?
+      puts "#{count} is even." if count.even?
+end 
---
+++ loops2/ex2.rb
@@ -0,0 +1,8 @@
+loop do
+  number = rand(100)
+  puts number
+  
+    if number.between?(0, 10)
+      break
+    end
+end
