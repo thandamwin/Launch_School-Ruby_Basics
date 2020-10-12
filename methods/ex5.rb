d2e7a21ecd3e989c460cef5d2db41de4c86eb871
Thanda Myo Win <thandamwin@gmail.com>
2020-10-12 15:16:01 +0000

    Methods

---
+++ methods/ex5.rb
@@ -0,0 +1,5 @@
+def car(type, model)
+    puts type + ' ' + model
+end
+
+car('Toyota', 'Corolla')
---
+++ methods/ex9.rb
@@ -0,0 +1,11 @@
+def add(a, b)
+    a + b
+end
+
+def multiply(c, d)
+    c * d
+end
+
+puts add(2, 2) == 4
+puts add(5, 4) == 9
+puts multiply(add(2, 2), add(5, 4)) == 36
